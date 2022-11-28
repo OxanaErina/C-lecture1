f22fab9a6a21cadb650e71cd9b648f49a3c38b25
Oxana Erina <oxana.erina@gmail.com>
2022-11-28 17:09:01 +0300

    Example005 added

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "оксана")
+{
+    Console.WriteLine("Ура, это же ОКСАНА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
